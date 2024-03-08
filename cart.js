b9d03bc9af903844c01dda9e085d053ffce9d34c
khan-shimul <khanshimul196@gmail.com>
2024-03-09 02:41:51 +0600

    first commit

---
+++ cart.js
@@ -0,0 +1,75 @@
+const addProduct = () => {
+    const productField = document.getElementById('product-name');
+    const product = productField.value;
+    const quantityField = document.getElementById('product-quantity');
+    const quantity = quantityField.value;
+    productField.value = '';
+    quantityField.value = '';
+
+    console.log(product, quantity)
+    displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
+}
+
+const displayProduct = (product, quantity) => {
+    const ul = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText = `${product}: ${quantity}`
+    ul.appendChild(li);
+}
+
+
+// const getStoredShoppingCart = () => {
+//     const storedCart = localStorage.getItem('cart');
+//     let cart = {};
+//     if(storedCart){
+//         cart = JSON.parse(storedCart);
+//     }
+//     return cart
+// }
+
+// const saveProductToLocalStorage = (product, quantity) => {
+//     const cart = getStoredShoppingCart();
+//     cart[product] = quantity;
+//     console.log(cart);
+//     const cartStringified = JSON.stringify(cart);
+//     localStorage.setItem('cart', cartStringified);
+// }
+
+// const displayProductFromLocalStorage = () => {
+//     const savedCart = getStoredShoppingCart();
+//     console.log(savedCart)
+//     for(const product in savedCart) {
+//         const quantity = savedCart[product];
+//         console.log(product, quantity)
+//         displayProduct(product, quantity)
+//     }
+// }
+
+// displayProductFromLocalStorage()
+
+
+
+const getStoredShoppingCart = () => {
+    const storedCart = localStorage.getItem('cart');
+    let cart = {};
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified)
+}
+
+const displayProductFromLocalStorage = () => {
+    const savedCart = getStoredShoppingCart();
+    for(const product in savedCart) {
+        const quantity = savedCart[product];
+        displayProduct(product, quantity)
+    }
+}
+displayProductFromLocalStorage()
